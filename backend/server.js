9b2a256c5ffabeb44bc44164d838d5de2b5a2fe6
SteveTheKey <william.s.matiz@gmail.com>
2023-02-20 12:26:52 -0500

    Add colors to log messages

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
+
 
 const app = express()
 dotenv.config()
@@ -22,4 +24,4 @@ app.get('/api/products/:id', (req, res) =>{
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}/api/products/`))
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}/api/products/`.cyan.bold))
