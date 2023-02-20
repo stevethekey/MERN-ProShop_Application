608fc3c01cadb8a2055ecf25c5a94bce8335d489
SteveTheKey <william.s.matiz@gmail.com>
2023-02-20 14:01:01 -0500

    Add script to populate MongoDB database

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,21 @@ import mongoose from "mongoose"
 
 const connectDB = async () =>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, /*{
+            
             useUnifiedTopology: true,
             useNewUrlParser: true,
-            useCreateIndex: true
-        })
+            useCreateIndex: true 
+
+            The useCreateIndex option has been deprecated for a while and removed as of the Mongoose 6 release. 
+            UseNewUrlParser , useUnifiedTopology , useFindAndModify , and useCreateIndex are no longer supported options. 
+            Mongoose 6 always behaves as if useNewUrlParser , useUnifiedTopology , and useCreateIndex are true , 
+            and useFindAndModify is false
+
+            Source: https://www.mongodb.com/community/forums/t/option-usecreateindex-is-not-supported/123048/3
+
+            In other words, no need to do this code block
+        }*/)
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch(error){
--- backend/data/users.js
+++ backend/data/users.js
@@ -5,21 +5,20 @@ const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: bycrpt.hashSync('1234567', 10),
+        password: bcrypt.hashSync('1234567', 10),
         isAdmin: true
     },
 
     {
         name: 'Cristiano Ronaldo',
         email: 'CR7@example.com',
-        password: bycrpt.hashSync('1234567', 10),
+        password: bcrypt.hashSync('1234567', 10),
     },
 
     {
         name: 'Lebron James',
         email: 'KingJames@example.com',
-        password: bycrpt.hashSync('1234567', 10),
-        isAdmin: true
+        password: bcrypt.hashSync('1234567', 10),
     },
 ]
 
