a428d4569a0bd1a63f7f9aad63386653a4618cce
SteveTheKey <william.s.matiz@gmail.com>
2023-02-20 12:21:22 -0500

    Set up mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 const app = express()
 dotenv.config()
+
+connectDB()
 
 app.get('/', (req, res) =>{
     res.send('API is running. Hi!')
