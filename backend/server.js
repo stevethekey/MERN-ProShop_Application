580bd70a1c2a216d69449296cff4f6153b09871c
SteveTheKey <william.s.matiz@gmail.com>
2023-02-20 11:36:49 -0500

    Set up .env and added useful environemtn variables

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 //const express = require is common nodeJS
 
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
 const app = express()
+
+dotenv.config()
 
 app.get('/', (req, res) =>{
     res.send('API is running. Hi!')
@@ -17,5 +20,5 @@ app.get('/api/products/:id', (req, res) =>{
     res.json(product)
 })
 
-
-app.listen(5000, console.log('Server is running on http://localhost:5000/api/products/'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}/api/products/`))
